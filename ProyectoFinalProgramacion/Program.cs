dcb6997882d2ef7ff9898aae21b40b48483b9820
NicoML4 <nicorayococo@gmail.com>
2025-04-08 12:23:50 +0200

    Borrado completo

--- ProyectoFinalProgramacion/Program.cs
+++ ProyectoFinalProgramacion/Program.cs
@@ -4,11 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Esto es una prueba para git");
-            Console.WriteLine("Jose");
-            Console.WriteLine("Miguel");
-            Console.WriteLine("pruebas Nico");
-            Console.WriteLine("Hola buenas");
 
         }
     }
