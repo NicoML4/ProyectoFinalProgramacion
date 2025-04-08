c95aeefba16dd0d414852cb87a586e0a225f5ddc
NicoML4 <nicorayococo@gmail.com>
2025-04-08 12:37:17 +0200

    Commit malo

--- ProyectoFinalProgramacion/Program.cs
+++ ProyectoFinalProgramacion/Program.cs
@@ -4,13 +4,11 @@
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Esto es una prueba para git");
             Console.WriteLine("Jose");
             Console.WriteLine("Miguel");
             Console.WriteLine("pruebas Nico");
             Console.WriteLine("Hola buenas");
-            Console.WriteLine("sisisis");
         }
     }
 }
