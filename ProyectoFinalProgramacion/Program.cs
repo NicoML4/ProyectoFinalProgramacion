ac5dbf6f74290b2c5f7e311763dee3f1049a6211
Miguel26-cat <migtorlop@alu.edu.gva.es>
2025-04-10 12:50:20 +0200

    Creación del main y clase usuario

--- ProyectoFinalProgramacion/Program.cs
+++ ProyectoFinalProgramacion/Program.cs
@@ -2,13 +2,72 @@
 {
     internal class Program
     {
+
+        public static void comprobarExistenciaUsuario()
+        {
+            string nombre;
+            string contrasena;
+
+            File.ReadAllLines("");
+        }
+
+        public static void login()
+        {
+            int eleccion;
+            bool valido = false;
+            while(!valido)
+            { 
+                Console.WriteLine("1.Iniciar Sesión \n");
+
+                Console.WriteLine("2.Registrarse");
+
+                eleccion = Convert.ToInt32(Console.ReadLine());
+
+                switch (eleccion)
+                {
+                    case 1:
+                        comprobarExistenciaUsuario();
+                        break;
+
+                    case 2:
+
+
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion no válida");
+                        break;
+
+                }
+            }
+        }
+        public static void menuOpciones()
+        {
+            Console.WriteLine("1. Abrir sobres");
+            Console.WriteLine("2. Album");
+            Console.WriteLine("3. Ajustes");
+            Console.Write("Introduce una opción");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            switch(opcion)
+            {
+                case 1:
+                    abrirSobres();
+                    break;
+                case 2:
+                    Album()
+                    break;
+                case 3:
+                    ajustes()
+                    break;
+            }    
+        }
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Esto es una prueba para git");
-            Console.WriteLine("Jose");
-            Console.WriteLine("Miguel");
-            Console.WriteLine("pruebas Nico");
-            Console.WriteLine("Hola buenas");
+            login();
+            menuOpciones();
         }
     }
 }
---
+++ ProyectoFinalProgramacion/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalProgramacion
+{
+    internal class Usuario
+    {
+        string nombreUsuario;
+        string contrasena;
+
+        public Usuario(string nombreUsuario, string contrasena)
+        { 
+            this.nombreUsuario = nombreUsuario;
+            this.contrasena = contrasena;
+        }
+    }
+}
